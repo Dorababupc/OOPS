8737e7b719ff6be22fbf3b2bf3615b4f4f704e5e
Dorababu Arigi <87510464+Dorababupc@users.noreply.github.com>
2022-06-02 10:04:59 +0530

    Update 4-constructor_in_cpp.cpp

--- 4-constructor_in_cpp.cpp
+++ 4-constructor_in_cpp.cpp
@@ -18,7 +18,9 @@ class Rectangle
     }
 };
 int main(){
-    //use only r,not r( ) in case of non parametrised.
+    //use only r,not r( ) incase of non parametrised or default constructor.
+    //Even if we give r() then this line will not be an error. Because compiler thinks this as the function prototype.
+    //And the object will not be created so you will get an error at the point where we try to use r as an object.
     Rectangle r;
     cout<<r.breadth<<endl;
 }
