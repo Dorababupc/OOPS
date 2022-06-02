19f3e90ce4113dfa2d982a67dcc0814338777e0c
Dorababu Arigi <87510464+Dorababupc@users.noreply.github.com>
2022-06-02 10:07:13 +0530

    Update 5-parametrised_constructor_in_cpp.cpp

--- 5-parametrised_constructor_in_cpp.cpp
+++ 5-parametrised_constructor_in_cpp.cpp
@@ -20,5 +20,7 @@ class Rectangle
 int main(){
     //use only r,not r( ) in case of non parametrised.
     Rectangle r(1);
+    //The second paramter b will automatically be initialised as we have given the default paramter value for that.
+    //So, you can notice that this(parametrised constructor with default parameters can act as both parametrised and non parametrised constructor.
     cout<<r.breadth<<endl;
 }
