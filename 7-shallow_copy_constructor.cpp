cacead4f89f5497519f0162ead52821aa6fed5df
Dorababu Arigi <87510464+Dorababupc@users.noreply.github.com>
2022-06-02 10:21:25 +0530

    Update and rename 7-deep_copy_constructor.cpp to 7-shallow_copy_constructor.cpp

---
+++ 7-shallow_copy_constructor.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class Rectangle
+{
+    public:
+    int length;
+    int *breadth;
+    Rectangle(){
+        length=1;
+        breadth=new int[5];
+        for(int i=0;i<5;++i){
+            breadth[i]=-1;
+        }
+    }
+    Rectangle(Rectangle &x){
+        length=x.length;
+        breadth=x.breadth;
+    }
+    void showBreadth(){
+        for(int i=0;i<5;++i){
+            cout<<breadth[i]<<endl;
+        }
+    }
+};
+int main(){
+    Rectangle r;
+    Rectangle a(r);
+    cout<<r.showBreadth()<<endl;
+    cout<<a.showBreadth()<<endl;
+    r.breadth[1]=10;
+    cout<<r.showBreadth()<<endl;
+    cout<<a.showBreadth()<<endl;
+
+    //Here we may notice that when  we are trying to change the values inside r, then values inside a are also changing, because of the dynamic memory allocation done 
+    //on a varible in class.
+    
+    //So, this is just the work done by the compiler with going much into the depth of the copying,-shallow copy constructor.
+}
